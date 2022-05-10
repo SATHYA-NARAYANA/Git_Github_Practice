33ce7298241e4f3ec6d332bb29d0671662809684
Sathya Narayana <ksathya1496@gmail.com>
2022-05-10 08:57:49 +0530

    list example 2

---
+++ day3/list2.py
@@ -0,0 +1,7 @@
+list2 = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(list2[4:6])
+
+#This will return the items from position 4 to 6.
+
+#Remember that the first item is position 0,
+#and note that the item in position 5 is NOT included
